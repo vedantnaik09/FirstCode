c6c19d3a6855e58c5248889881248a91dd515e7d
VedantLaptop <vedant.naik09@gmail.com>
2023-04-02 21:32:18 +0530

    2/4/23

--- src/main/kotlin/EntityFactory.kt
+++ src/main/kotlin/EntityFactory.kt
@@ -1,21 +1,55 @@
 import java.util.*
+import kotlin.math.E
 
+enum class EntityType{
+    EASY,MEDIUM,HARD;
+    fun getFormattedName()= name.lowercase().capitalize()
+}
 object EntityFactory{
-    fun create():Entity{
+    fun create(type: EntityType):Entity{
         val id= UUID.randomUUID().toString()
-        return Entity(id, "name")
+        val name=when(type){
+        EntityType.EASY-> type.name
+        EntityType.MEDIUM-> type.getFormattedName()
+        EntityType.HARD-> "Hard"
+        }
+        return Entity(id, name)
     }
 }
-class Entity(val id:String, val name:String){
+class Entity(val id:String, val  name:String){
     override fun toString(): String {
         return "id:$id, Name:$name "
     }
 
 }
 fun main(){
-    val entity=EntityFactory.create()
+    val entity=EntityFactory.create(EntityType.EASY)
     println(entity)
+    val mediumEntity=EntityFactory.create(EntityType.MEDIUM)
+    println(mediumEntity)
+    val hardEntity=EntityFactory.create(EntityType.HARD)
+    println(hardEntity)
+    println(EntityType.EASY.name)
 }
+
+//----------------------------------------------
+//object EntityFactory{
+//    fun create():Entity{
+//        val id= UUID.randomUUID().toString()
+//        return Entity(id, "name")
+//    }
+//}
+//class Entity(val id:String, val name:String){
+//    override fun toString(): String {
+//        return "id:$id, Name:$name "
+//    }
+//
+//}
+//fun main(){
+//    val entity=EntityFactory.create()
+//    println(entity)
+//}
+//--------------------------------------
 //object EntityFactory{
 //    fun create()=Entity("id")
 //}
